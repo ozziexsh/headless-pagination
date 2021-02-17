2a837e75b530567b7ae5bf12bf0287d76c33d51b
Ozzie Neher <oz.neher@gmail.com>
2021-02-17 10:49:15 -0600

    Add boundaries to fields, add option to update initial config

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -5,8 +5,64 @@ function App() {
   const [totalItems, setTotalItems] = useState(1000);
   const [perPage, setPerPage] = useState(24);
   const [maxLinks, setMaxLinks] = useState(7);
+  const { page, links, from, to, setPage } = usePagination({
+    totalItems,
+    perPage,
+    maxLinks,
+  });
 
-  return <div className="App"></div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <label htmlFor="total-items">Total items</label>
+          <input
+            type="number"
+            id="total-items"
+            value={totalItems}
+            onChange={e => setTotalItems(Number(e.currentTarget.value))}
+          />
+        </div>
+        <div>
+          <label htmlFor="per-page">Per page</label>
+          <input
+            type="number"
+            id="per-page"
+            value={perPage}
+            onChange={e => setPerPage(Number(e.currentTarget.value))}
+          />
+        </div>
+        <div>
+          <label htmlFor="max-links">Max links</label>
+          <input
+            type="number"
+            id="max-links"
+            value={maxLinks}
+            onChange={e => setMaxLinks(Number(e.currentTarget.value))}
+          />
+        </div>
+      </div>
+      <div>
+        <p>
+          Showing {from} to {to} of {totalItems}
+        </p>
+        <div>
+          {links.map((link, i) => (
+            <button
+              key={i}
+              disabled={link.disabled}
+              style={{ backgroundColor: link.active ? '#00aced' : undefined }}
+              onClick={() =>
+                typeof link.label !== 'string' ? setPage(link.label) : undefined
+              }
+            >
+              {link.label}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
